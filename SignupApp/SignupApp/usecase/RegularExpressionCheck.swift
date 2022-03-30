6506250dd06a875636c6ddb277b59582fc3cf78c
kimdongjune <jun36817@naver.com>
2022-03-30 16:37:02 +0900

    메소드명 변경 "객체.행동()"으로 리펙토링

--- SignupApp/SignupApp/usecase/RegularExpressionCheck.swift
+++ SignupApp/SignupApp/usecase/RegularExpressionCheck.swift
@@ -8,7 +8,7 @@
 import Foundation
 
 class RegularExpressionCheck{
-    private func isUsableId(id: String) -> IdVerifyState{
+    private func checkIDVerify(in id: String) -> IdVerifyState{
         let pattern = "^[a-z0-9_-]{5,20}$"
         let regex = try? NSRegularExpression(pattern: pattern)
         if let _ = regex?.firstMatch(in: id, options: [], range: NSRange(location: 0, length: id.count)) {
@@ -17,23 +17,23 @@ class RegularExpressionCheck{
         return .unable
     }
     
-    private func isUsablePassword(password: String) -> PasswordVerifyState{
-        if !isUsablePasswordLength(password: password){
-            return .length
+    private func checkPasswordVerify(in password: String) -> PasswordVerifyState{
+        if !satisfiedPasswordLength(in: password){
+            return .lengthRange
         }
-        if !isPasswordIncludeUpper(password: password){
-            return .upper
+        if !hasUppercase(in: password){
+            return .upperInclude
         }
-        if !isPasswordIncludeNumber(password: password){
-            return .number
+        if !hasNumbercase(in: password){
+            return .numberInclude
         }
-        if !isPasswordIncludeSpecialCharacter(password: password){
-            return .speical
+        if !hasSpecialCharcase(in: password){
+            return .speicalCharInclude
         }
         return .usable
     }
     
-    private func isUsablePasswordLength(password: String) -> Bool{
+    private func satisfiedPasswordLength(in password: String) -> Bool{
         if password.count >= 8 && password.count <= 16{
             return true
         }
@@ -41,7 +41,7 @@ class RegularExpressionCheck{
     }
     
     
-    private func isPasswordIncludeUpper(password: String) -> Bool{
+    private func hasUppercase(in password: String) -> Bool{
         let pattern = "(?=.*[A-Z])"
         let regex = try? NSRegularExpression(pattern: pattern)
         if let _ = regex?.firstMatch(in: password, options: [], range: NSRange(location: 0, length: password.count)) {
@@ -50,7 +50,7 @@ class RegularExpressionCheck{
         return false
     }
     
-    private func isPasswordIncludeNumber(password: String) -> Bool{
+    private func hasNumbercase(in password: String) -> Bool{
         let pattern = "(?=.*[0-9])"
         let regex = try? NSRegularExpression(pattern: pattern)
         if let _ = regex?.firstMatch(in: password, options: [], range: NSRange(location: 0, length: password.count)) {
@@ -59,7 +59,7 @@ class RegularExpressionCheck{
         return false
     }
     
-    private func isPasswordIncludeSpecialCharacter(password: String) -> Bool{
+    private func hasSpecialCharcase(in password: String) -> Bool{
         let pattern = "(?=.*[!@#$])"
         let regex = try? NSRegularExpression(pattern: pattern)
         if let _ = regex?.firstMatch(in: password, options: [], range: NSRange(location: 0, length: password.count)) {
@@ -71,19 +71,19 @@ class RegularExpressionCheck{
 
 extension RegularExpressionCheck: RegularExpressionCheckable{
     func verifyUsableId(id: String) -> IdVerifyState {
-        return isUsableId(id: id)
+        return checkIDVerify(in: id)
     }
     
     func verifyUsablePassword(password: String) -> PasswordVerifyState {
-        return isUsablePassword(password: password)
+        return checkPasswordVerify(in: password)
     }
 }
 
 enum PasswordVerifyState{
-    case length
-    case upper
-    case number
-    case speical
+    case lengthRange
+    case upperInclude
+    case numberInclude
+    case speicalCharInclude
     case usable
 }
 
